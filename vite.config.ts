8f56f055bf1413a2a602a0539acba52bd81ed942
B0canegraS <bocanegrasalvaa@gmail.com>
2025-12-24 19:27:54 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/GreekLink/',
+  base: '/Grikly/',
   server: {
     host: "::",
     port: 8080,
